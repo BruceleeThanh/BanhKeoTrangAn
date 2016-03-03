ed68487e940d6a25a4a23cf7df4ca77ebf93fc17
BruceleeThanh <nguyentrungthanh602@gmail.com>
2016-03-04 01:34:29 +0700

    Sửa webroot/index.php giúp tiện cho việc config

--- webroot/index.php
+++ webroot/index.php
@@ -1,14 +1,15 @@
 <?php
 
+define('ROOT_PATH', 'http://localhost:8080/BanhKeoTrangAn/');
+
 define('DS', DIRECTORY_SEPARATOR); // dau ' / '
 define('ROOT', dirname(dirname(__FILE__))); // thu muc cha
 
 define('VIEWS_PATH', ROOT . DS . 'views');
-define('WEBROOT_PATH', 'http://localhost:8080/BanhKeoTrangAn/webroot');
-define('ROOT_PATH', 'http://localhost:8080/BanhKeoTrangAn/');
-define('ADMIN_ROOT', 'http://localhost:8080/BanhKeoTrangAn/admin');
-define('DEFAULT_ROOT', 'http://localhost:8080/BanhKeoTrangAn/default');
-define('INDEX', 'http://localhost:8080/BanhKeoTrangAn/en/index');
+define('WEBROOT_PATH', ROOT_PATH . 'webroot');
+define('ADMIN_ROOT', ROOT_PATH . 'admin');
+define('DEFAULT_ROOT', ROOT_PATH . 'default');
+define('INDEX', ROOT_PATH . 'en/index');
 
 define('SLIDEBAR_MENU', dirname(dirname(__FILE__)) . DS . "views\_layout_admin\sidebar_menu.php");
 define('LAYOUT_ADMIN', dirname(dirname(__FILE__)) . DS . "views\_layout_admin");
@@ -16,7 +17,5 @@ define('LAYOUT_ADMIN', dirname(dirname(__FILE__)) . DS . "views\_layout_admin");
 require_once (ROOT . DS . 'lib' . DS . 'init.php');
 
 session_start();
-//print_r($_SERVER['REQUEST_URI']);
-//die;
 App::run($_SERVER['REQUEST_URI']);
 
